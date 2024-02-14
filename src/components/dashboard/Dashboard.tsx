e152b97bd37a91153c03a55499b66083d2f7f523
Uladzislau Lailo <ULailo@iba.by>
2024-02-14 03:55:11 +0300

    DASH-11 timezone selection fix, elements removing fix

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,11 +4,12 @@ import React, {Fragment} from "react";
 import {DashboardNavbar} from "./dashboard-navbar";
 import {EmptyElement} from "./empty-element";
 import {Grid, GridElement} from "../grid";
+import {BasicElementProps} from "./basic-element/BasicElement";
 
 export type DashboardProps = {}
 
 export type DashboardState = {
-    elements: React.ReactElement[]
+    elements: React.ReactElement<BasicElementProps>[]
 }
 
 class Dashboard extends React.Component<DashboardProps, DashboardState> {
@@ -21,14 +22,14 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
         };
     }
 
-    setElements(elements: React.ReactElement[]) {
+    setElements(elements: React.ReactElement<BasicElementProps>[]) {
         this.setState((state) => ({
             ...state,
             elements: elements
         }));
     }
 
-    addElement(element: React.ReactElement) {
+    addElement(element: React.ReactElement<BasicElementProps>) {
         const {elements} = this.state;
 
         this.setElements(elements.concat(element))
@@ -51,8 +52,8 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
                 <div className={"dashboard"}>
                     <Grid rows={cells} cols={cells} cellSize={10}>
                         {
-                            elements.map((el, index) =>
-                                <GridElement row={1} col={1} width={40} height={40} key={index}>{el}</GridElement>)
+                            elements.map((el) =>
+                                <GridElement row={1} col={1} width={40} height={40} key={el.props.uniqueKey}>{el}</GridElement>)
                         }
                         <GridElement row={1} col={1} width={30} height={20}>
                             <EmptyElement dashboard={this}/>
